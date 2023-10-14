0dda04822af32798b0e470e33dc62832bb406c83
AYAMBA Leonard NDIFON <layamba25@gmail.com>
2023-10-14 16:36:54 -0400

    update action file

--- BashScripts/splunk_installer.sh
+++ BashScripts/splunk_installer.sh
@@ -2,12 +2,12 @@
 
 # Ensure the script is run with root privileges
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
+    echo "###### This script must be run as root #######"
     exit 1
 fi
 
 # 1. Create a splunk_user with sudo privileges and no password
-echo "Creating splunk_user with sudo privileges and no password"
+echo "###### Creating splunk_user with sudo privileges and no password ########"
 sudo useradd -m -s /bin/bash splunk_user
 echo "splunk_user ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
@@ -18,7 +18,7 @@ wget -O /tmp/splunk.tgz https://download.splunk.com/products/splunk/releases/9.1
 tar -xzvf /tmp/splunk.tgz -C /opt/
 
 # 4.0 Add Default Splunk Admin user and password
-echo "Adding default Splunk Admin user and password"
+echo "###### Adding default Splunk Admin user and password ########"
 
 cat > /opt/splunk/etc/system/local/user-seed.conf << 'EOF'
 [user_info]
@@ -33,12 +33,12 @@ minFreeSpace = 0
 EOF
 
 # 4.2 Change ownership
-echo "Changing ownership of /opt/splunk to splunk_user"
+echo "############ Changing ownership of /opt/splunk to splunk_user #############"
 sudo chown -R splunk_user:splunk_user /opt/splunk
 # sleep 2s
 
 # 5. Start Boot start
-echo "Enabling Splunk to start at boot via Systemd"
+echo "############# Enabling Splunk to start at boot via Systemd ##############"
 /opt/splunk/bin/splunk enable boot-start -systemd-managed 1 -user splunk_user --accept-license --answer-yes --no-prompt
 
 # sleep 2s
@@ -46,18 +46,18 @@ echo "Enabling Splunk to start at boot via Systemd"
 sudo chown -R splunk_user:splunk_user /opt/splunk
 
 # 7. Reload Systemd
-echo "Reloading Systemd"
+echo "############## Reloading Systemd ###############"
 systemctl daemon-reload
 
-echo "Enabling Splunkd"
+echo "############## Enabling Splunkd ##############"
 systemctl enable Splunkd
 
-echo "Starting Splunkd"
+echo "############### Starting Splunkd ###############"
 systemctl start Splunkd
 
 # Check Splunkd status
 sleep 5s
-echo "Checking Splunkd status"
+echo "############### Checking Splunkd status ################"
 # systemctl status Splunkd
 if sudo systemctl status Splunkd | grep -q "active (running)"; then
     echo "Splunkd is running"
