e51c5a21b25f5471363bf17a6e4503c71e2c02fd
AYAMBA Leonard NDIFON <layamba25@gmail.com>
2023-12-18 20:28:29 -0500

    ansible commit

--- AWS/Terraform/Ansible/main.tf
+++ AWS/Terraform/Ansible/main.tf
@@ -37,26 +37,26 @@ resource "aws_instance" "ansible" {
             EOF
 
     # Use remote-exec provisioner to create ansible user and generate private key
-    provisioner "remote-exec" {
-        inline = [
-            "sudo useradd -m -s /bin/bash ansible",
-            "echo 'ansible ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers.d/ansible-sudoers",
-            "sudo mkdir -p /home/ansible/.ssh",
-            "sudo chmod 700 /home/ansible/.ssh",
-            "sudo touch /home/ansible/.ssh/authorized_keys",
-            "sudo chmod 600 /home/ansible/.ssh/authorized_keys",
-            "sudo chown -R ansible:ansible /home/ansible/.ssh",
-            "sudo -u ansible ssh-keygen -t rsa -N '' -f /home/ansible/.ssh/id_rsa",
-            "sudo cat /home/ansible/.ssh/id_rsa.pub"
-        ]
+    # provisioner "remote-exec" {
+    #     inline = [
+    #         "sudo useradd -m -s /bin/bash ansible",
+    #         "echo 'ansible ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers.d/ansible-sudoers",
+    #         "sudo mkdir -p /home/ansible/.ssh",
+    #         "sudo chmod 700 /home/ansible/.ssh",
+    #         "sudo touch /home/ansible/.ssh/authorized_keys",
+    #         "sudo chmod 600 /home/ansible/.ssh/authorized_keys",
+    #         "sudo chown -R ansible:ansible /home/ansible/.ssh",
+    #         "sudo -u ansible ssh-keygen -t rsa -N '' -f /home/ansible/.ssh/id_rsa",
+    #         "sudo cat /home/ansible/.ssh/id_rsa.pub"
+    #     ]
 
-         connection {
-            type        = "ssh"
-            user        = "ubuntu"
-            private_key = file("C:\\Users\\leona\\OneDrive\\Desktop\\Training\\Splunk Engineering\\AWS\\SplunkTraining.pem")
-            host        = self.public_ip
-        }
-    }
+    #      connection {
+    #         type        = "ssh"
+    #         user        = "ubuntu"
+    #         private_key = file("C:\\Users\\leona\\OneDrive\\Desktop\\Training\\Splunk Engineering\\AWS\\SplunkTraining.pem")
+    #         host        = self.public_ip
+    #     }
+    # }
 
     # Use file provisioner to copy the public key to the local machine
     # provisioner "file" {
